bf3ef78d477d7ebda80fa487cb832c378622e99e
Necati Doğrul <66124061+necatidogrul@users.noreply.github.com>
2024-05-26 12:27:27 +0300

    Add files via upload
    The real codes are here: https://github.com/waroi/TurkcellFrontend2024/tree/main/Ogrenciler/NecatiDogrul/Odevler/Vize%20-%202

---
+++ Skoob/app.js
@@ -0,0 +1,100 @@
+const addBookButton = document.querySelector("#addBookButton");
+let books = [];
+document.addEventListener("DOMContentLoaded", () => {
+  UI.displayBooks();
+});
+
+document.querySelector("#form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const category = document.querySelector("#category").value;
+  const publishedDate = document.querySelector("#publishedDate").value;
+  const coverImageUrl = document.querySelector("#coverImageUrl").value;
+  const bookCount = await UI.getBookCount();
+  let id = bookCount + 1;
+  id = id.toString();
+  const newBook = new Book(
+    id,
+    title,
+    author,
+    category,
+    publishedDate,
+    coverImageUrl
+  );
+
+  const book = await UI.createBook(newBook);
+  if (book) {
+    books.push(book);
+    UI.createCard(book); // Yeni kitabı UI'a ekle
+    document.querySelector("#form").reset(); // Formu temizle
+    // Modal'ı kapat
+    const addModal = bootstrap.Modal.getInstance(
+      document.getElementById("exampleModal")
+    );
+    if (addModal) {
+      addModal.hide();
+    }
+  }
+});
+
+document
+  .querySelector("#updateForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const id = document
+      .querySelector("#updateBookButton")
+      .getAttribute("data-id");
+    const title = document.querySelector("#updateTitle").value;
+    const author = document.querySelector("#updateAuthor").value;
+    const category = document.querySelector("#updateCategory").value;
+    const publishedDate = document.querySelector("#updatePublishedDate").value;
+    const coverImageUrl = document.querySelector("#updateCoverImageUrl").value;
+    const updatedBook = {
+      id,
+      title,
+      author,
+      category,
+      publishedDate,
+      coverImageUrl,
+    };
+    const success = await UI.updateBook(id, updatedBook);
+    if (success) {
+      document.querySelector("#cards").textContent = "";
+      UI.displayBooks(); // Kitapları yeniden yükle
+      // Modal'ı kapat
+      const updateModal = bootstrap.Modal.getInstance(
+        document.getElementById("updateModal")
+      );
+      if (updateModal) {
+        updateModal.hide();
+      }
+    }
+  });
+
+document.querySelector("#sortDropdown").addEventListener("change", (e) => {
+  const sortCriteria = e.target.value;
+  UI.displayBooks(sortCriteria);
+});
+
+document.querySelector("#filterForm").addEventListener("keyup", function (e) {
+  e.preventDefault();
+  const category = document
+    .querySelector("#filterCategory")
+    .value.trim()
+    .toLowerCase();
+  const author = document
+    .querySelector("#filterAuthor")
+    .value.trim()
+    .toLowerCase();
+  UI.filterBooks(category, author);
+});
+
+document.querySelector("#search").addEventListener("keyup", function (e) {
+  e.preventDefault();
+  const searchText = document
+    .querySelector("#search")
+    .value.trim()
+    .toLowerCase();
+  UI.searchBooks(searchText);
+});
---
+++ Skoob/ui.js
@@ -0,0 +1,248 @@
+class UI {
+  static createElement(tag, classes = [], appendChilds = [], textContent) {
+    const element = document.createElement(tag);
+    element.textContent = textContent;
+    classes.forEach((className) => element.classList.add(className));
+    appendChilds.forEach((appendChild) => element.appendChild(appendChild));
+    return element;
+  }
+
+  static createCard(book) {
+    const cards = document.querySelector("#cards");
+    const coverImage = this.createElement("img", [
+      "card-img-top",
+      "object-fit-contain",
+    ]);
+    coverImage.style.width = "100%";
+    coverImage.style.height = "100%";
+    coverImage.src = book.coverImageUrl;
+    const title = this.createElement("h5", ["card-title"], [], book.title);
+    const author = this.createElement("p", ["card-text"], [], book.author);
+    const category = this.createElement("p", ["card-text"], [], book.category);
+    const publishedDate = this.createElement(
+      "p",
+      ["card-text"],
+      [],
+      book.publishedDate
+    );
+    const deleteButton = this.createElement(
+      "button",
+      ["btn", "btn-danger", "btn-md"],
+      [],
+      "Delete"
+    );
+    deleteButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.deleteBook(book.id);
+    });
+    const updateButton = this.createElement(
+      "a",
+      ["btn", "btn-primary", "btn-md", "mx-2"],
+      [],
+      "Update"
+    );
+    updateButton.addEventListener("click", () => {
+      this.showUpdateModal(book);
+    });
+    const cardBody = this.createElement(
+      "div",
+      ["card-body", "col-md-6", "p-md-1", "my-auto"],
+      [title, author, category, publishedDate, updateButton, deleteButton]
+    );
+    const imageDiv = this.createElement("div", ["col-md-6"], [coverImage]);
+    const row = this.createElement(
+      "div",
+      ["row", "my-auto"],
+      [imageDiv, cardBody]
+    );
+    const card = this.createElement(
+      "div",
+      ["card", "col-md-5", "col-sm-10", "bg-light", "flex-grow-sm-1"],
+      [row]
+    );
+    card.setAttribute("data-id", book.id);
+    cards.appendChild(card);
+  }
+
+  static displayBooks(booksArray) {
+    booksArray.forEach((book) => {
+      const newBook = new Book(
+        book.id,
+        book.title,
+        book.author,
+        book.category,
+        book.publishedDate,
+        book.coverImageUrl
+      );
+      UI.createCard(newBook);
+    });
+  }
+
+  static removeBookFromUI(id) {
+    const bookCard = document.querySelector(`[data-id="${id}"]`);
+    if (bookCard) {
+      bookCard.remove();
+    }
+  }
+
+  static deleteBook(id) {
+    Request.delete(`http://localhost:3000/books/${id}`)
+      .then((response) => {
+        if (response.ok) {
+          this.removeBookFromUI(id);
+          console.log(`Book with ID ${id} has been deleted`);
+        } else {
+          console.log(`Failed to delete book with ID ${id}`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static async getBookCount() {
+    const allBooks = await Request.get("http://localhost:3000/books");
+    return allBooks.length;
+  }
+
+  static async createBook(book) {
+    try {
+      const response = await Request.post("http://localhost:3000/books", book);
+      console.log(`Book with ID ${response.id} has been created`);
+      return response;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  }
+
+  static showUpdateModal(book) {
+    document.querySelector("#updateTitle").value = book.title;
+    document.querySelector("#updateAuthor").value = book.author;
+    document.querySelector("#updateCategory").value = book.category;
+    document.querySelector("#updatePublishedDate").value = book.publishedDate;
+    document.querySelector("#updateCoverImageUrl").value = book.coverImageUrl;
+    document
+      .querySelector("#updateBookButton")
+      .setAttribute("data-id", book.id);
+    new bootstrap.Modal(document.getElementById("updateModal")).show();
+  }
+
+  static async updateBook(id, updatedBook) {
+    try {
+      const response = await Request.put(
+        `http://localhost:3000/books/${id}`,
+        updatedBook
+      );
+      console.log(`Book with ID ${id} has been updated`);
+      const bookCard = document.querySelector(`[data-id="${id}"]`);
+      if (bookCard) {
+        bookCard.querySelector(".card-title").textContent = updatedBook.title;
+        bookCard.querySelector(".card-text:nth-of-type(1)").textContent =
+          updatedBook.author;
+        bookCard.querySelector(".card-text:nth-of-type(2)").textContent =
+          updatedBook.category;
+        bookCard.querySelector(".card-text:nth-of-type(3)").textContent =
+          updatedBook.publishedDate;
+        bookCard.querySelector(".card-img-top").src = updatedBook.coverImageUrl;
+      }
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  static filterBooks(category, author) {
+    Request.get("http://localhost:3000/books")
+      .then((books) => {
+        let filteredBooks = books;
+        if (category) {
+          filteredBooks = filteredBooks.filter((book) =>
+            book.category.toLowerCase().includes(category.toLowerCase())
+          );
+        }
+        if (author) {
+          filteredBooks = filteredBooks.filter((book) =>
+            book.author.toLowerCase().includes(author.toLowerCase())
+          );
+        }
+        document.querySelector("#cards").innerHTML = "";
+        filteredBooks.forEach((book) => {
+          const newBook = new Book(
+            book.id,
+            book.title,
+            book.author,
+            book.category,
+            book.publishedDate,
+            book.coverImageUrl
+          );
+          UI.createCard(newBook);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static sortBooks(books, criteria) {
+    return books.sort((a, b) => {
+      if (criteria === "titleAZ") {
+        return a.title.localeCompare(b.title);
+      } else if (criteria === "titleZA") {
+        return b.title.localeCompare(a.title);
+      } else if (criteria === "dateAsc") {
+        return new Date(a.publishedDate) - new Date(b.publishedDate);
+      } else if (criteria === "dateDesc") {
+        return new Date(b.publishedDate) - new Date(a.publishedDate);
+      }
+      return 0;
+    });
+  }
+
+  static displayBooks(sortCriteria) {
+    Request.get("http://localhost:3000/books").then((books) => {
+      if (sortCriteria) {
+        books = UI.sortBooks(books, sortCriteria);
+      }
+      document.querySelector("#cards").innerHTML = "";
+      books.forEach((book) => {
+        const newBook = new Book(
+          book.id,
+          book.title,
+          book.author,
+          book.category,
+          book.publishedDate,
+          book.coverImageUrl
+        );
+        UI.createCard(newBook);
+      });
+    });
+  }
+
+  static searchBooks(searchText) {
+    Request.get("http://localhost:3000/books")
+      .then((books) => {
+        const filteredBooks = books.filter(
+          (book) =>
+            book.title.toLowerCase().includes(searchText) ||
+            book.author.toLowerCase().includes(searchText)
+        );
+        document.querySelector("#cards").innerHTML = "";
+        filteredBooks.forEach((book) => {
+          const newBook = new Book(
+            book.id,
+            book.title,
+            book.author,
+            book.category,
+            book.publishedDate,
+            book.coverImageUrl
+          );
+          UI.createCard(newBook);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
